d7411854e8a98438dbac8de8d279690880e87763
stravze <stravze@gmail.com>
2019-05-31 09:35:09 +0100

    Setup

--- 01-inputs-required.tf
+++ 01-inputs-required.tf
@@ -1,1 +1,42 @@
 #
+
+variable "rg_name" {
+  description = "Name of the rg to put the network in"
+}
+
+variable "rg_location" {
+    description = "Azure Location"
+}
+
+variable "proxy_vm_name" {
+    description = "Proxy VM Name"
+}
+
+
+variable "" {
+    description = ""
+}
+
+
+variable "" {
+    description = ""
+}
+
+
+variable "" {
+    description = ""
+}
+
+
+variable "" {
+    description = ""
+}
+
+variable "" {
+    description = ""
+}
+
+
+variable "" {
+    description = ""
+}
---
+++ 15-vm.tf
@@ -0,0 +1,101 @@
+
+resource "azurerm_virtual_machine" "proxy_vm" {
+  name                                    = "${var.proxy_vm_name}"
+  location                                = "${var.rg_location}"
+  resource_group_name                     = "${var.rg_name}"
+  network_interface_ids                   = ["${azurerm_network_interface.proxy_nic.id}"]
+  vm_size                                 = "Standard_B1s"
+  delete_os_disk_on_termination = true
+  storage_image_reference {
+    publisher                             = "Canonical"
+    offer                                 = "UbuntuServer"
+    sku                                   = "18.04-LTS"
+    version                               = "latest"
+  }
+
+  storage_os_disk {
+    name                                  = "${var.proxy_vm_name}-os"
+    caching                               = "ReadWrite"
+    create_option                         = "FromImage"
+    managed_disk_type                     = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name                         = "${var.proxy_vm_name}"
+    admin_username                        = "${var.proxy_admin_username}"
+    admin_password                        = "${var.proxy_admin_password}"
+  }
+
+provisioner "remote-exec" {
+      inline = [
+        "mkdir ~/ansible"
+      ]
+        connection {
+      type                                = "ssh"
+      user                                = "${var.proxy_admin_username}"
+      password                            = "${var.proxy_admin_password}"
+      }
+}
+
+ os_profile_linux_config {
+    disable_password_authentication       = false
+
+  }
+
+}
+
+resource "azurerm_virtual_machine_extension" "ansible_extension" {
+  name                                    = "Ansible-Agent-Install"
+  location                                = "${var.rg_location}"
+  resource_group_name                     = "${var.rg_name}"
+  virtual_machine_name                    = "${azurerm_virtual_machine.proxy_vm.name}"
+  publisher                               = "Microsoft.Azure.Extensions"
+  type                                    = "CustomScript"
+  type_handler_version                    = "2.0"
+
+  settings = <<SETTINGS
+    {
+        "commandToExecute": "sudo apt-get install -y software-properties-common",
+        "commandToExecute": "sudo apt-add-repository --yes --update ppa:ansible/ansible",
+        "commandToExecute": "sudo apt-get install -y ansible"
+    }
+SETTINGS
+}
+
+
+resource "null_resource" "ansible-runs" {
+    triggers = {
+      always_run = "${timestamp()}"
+    }
+
+    depends_on = [
+        "azurerm_virtual_machine_extension.ansible_extension",
+        "azurerm_virtual_machine.proxy_vm"
+    ]
+
+  provisioner "file" {
+    source                                = "${path.module}/ansible/"
+    destination                           = "~/ansible/"
+
+    connection {
+      type                                = "ssh"
+      user                                = "${var.proxy_admin_username}"
+      password                            = "${var.proxy_admin_password}"
+      host                                = "${azurerm_public_ip.proxy_pip.*.ip_address}"
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "ansible-galaxy install -r ~/ansible/requirements.yml",
+      "ansible-playbook ~/ansible/playbooks/playbook.yml"
+    ]
+
+    connection {
+      type                                = "ssh"
+      user                                = "${var.proxy_admin_username}"
+      password                            = "${var.proxy_admin_password}"
+      host                                = "${azurerm_public_ip.proxy_pip.*.ip_address}"
+    }
+  }
+}
