9effc9a861769eb63c500cfd5ac7da04762a62a7
stravze <stravze@gmail.com>
2019-06-03 11:06:55 +0100

    subnet issue

--- 11-network-interfaces.tf
+++ 11-network-interfaces.tf
@@ -1,12 +1,12 @@
 # Disabled as using F5 provisioned subnets
 
-resource "azurerm_subnet" "subnet" {
-  name                                     = "${var.proxy_vm_name}-subnet"
-  resource_group_name                      = "${var.rg_name}"
-  virtual_network_name                     = "${var.vnet_name}"
-  address_prefix                           = ["${var.address_prefix}"]
+#resource "azurerm_subnet" "subnet" {
+#  name                                     = "${var.proxy_vm_name}-subnet"
+#  resource_group_name                      = "${var.rg_name}"
+#  virtual_network_name                     = "${var.vnet_name}"
   #address_prefix                           = "${element(azurerm_subnet.subnet.*.id, index(azurerm_subnet.subnet.*.name, var.proxy_subnet_vip))}"
-}
+#  address_prefix                           = "${var.address_prefix}"
+#}
 
 resource "azurerm_public_ip" "proxy_pip" {
    name                                     = "${var.proxy_vm_name}-pip"
@@ -23,7 +23,7 @@ resource "azurerm_network_interface" "proxy_nic" {
 
   ip_configuration {
     name                                    = "${var.proxy_vm_name}-ip"
-    subnet_id                               = ["${element(azurerm_subnet.subnet.*.id, index(azurerm_subnet.subnet.*.name, var.proxy_subnet_vip))}"]
+    subnet_id                               = "${var.proxy_subnet_vip}"
     private_ip_address_allocation           = "Dynamic"
     public_ip_address_id                    = "${element(azurerm_public_ip.proxy_pip.*.id, count.index)}"
   }
