b90a11b53c245bd6e8c0885f0c7484e39d148446
stravze <stravze@gmail.com>
2019-06-03 10:28:31 +0100

    block testing

--- 11-network-interfaces.tf
+++ 11-network-interfaces.tf
@@ -4,7 +4,7 @@ resource "azurerm_subnet" "subnet" {
   name                                     = "${var.proxy_vm_name}-subnet"
   resource_group_name                      = "${var.rg_name}"
   virtual_network_name                     = "${var.vnet_name}"
-  address_prefix                           = "${var.address_prefix}"
+  address_prefix                           = ["${var.address_prefix}"]
   #address_prefix                           = "${element(azurerm_subnet.subnet.*.id, index(azurerm_subnet.subnet.*.name, var.proxy_subnet_vip))}"
 }
 
@@ -23,7 +23,7 @@ resource "azurerm_network_interface" "proxy_nic" {
 
   ip_configuration {
     name                                    = "${var.proxy_vm_name}-ip"
-    subnet_id                               = "${element(azurerm_subnet.subnet.*.id, index(azurerm_subnet.subnet.*.name, var.proxy_subnet_vip))}"
+    subnet_id                               = ["${element(azurerm_subnet.subnet.*.id, index(azurerm_subnet.subnet.*.name, var.proxy_subnet_vip))}"]
     private_ip_address_allocation           = "Dynamic"
     public_ip_address_id                    = "${element(azurerm_public_ip.proxy_pip.*.id, count.index)}"
   }
