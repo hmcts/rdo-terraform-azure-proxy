e14c9174183fbedca9a2a7064c2b23b5ed30deeb
stravze <stravze@gmail.com>
2019-07-12 14:26:58 +0100

    typo

--- 15-vm.tf
+++ 15-vm.tf
@@ -92,7 +92,7 @@ resource "null_resource" "ansible-runs" {
     inline = [
       "hostname > ~/ansible/hosts",
       "cd ~/ansible",
-      "sudo ansible-playbook ~/ansible/proxy.yml -i ~/ansible/hosts -e arm_tennat_id=${var.arm_tennat_id} -e arm_client_id=${var.arm_client_id} -e arm_client_secret=${var.arm_client_secret}",
+      "sudo ansible-playbook ~/ansible/proxy.yml -i ~/ansible/hosts -e arm_tenant_id=${var.arm_tenant_id} -e arm_client_id=${var.arm_client_id} -e arm_client_secret=${var.arm_client_secret}",
     ]
 
     connection {
