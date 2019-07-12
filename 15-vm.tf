2dc1c0edeb7bb011ca15eb166730e8d865127c3f
stravze <stravze@gmail.com>
2019-07-12 11:24:37 +0100

    testing

--- 15-vm.tf
+++ 15-vm.tf
@@ -90,10 +90,13 @@ resource "null_resource" "ansible-runs" {
 
   provisioner "remote-exec" {
     inline = [
-      "curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash",
-      "az --version",
+      #"curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash",
+      #"az --version",
       "echo ${var.subscription_id}",
-      "echo sudo az login --service-principal --username $(ARM_CLIENT_ID) --password $(ARM_CLIENT_SECRET) --tenant $(ARM_TENANT_ID)",
+      "echo ${var.subscription_id}",
+      "echo ${var.subscription_id}",
+      "echo ${var.subscription_id}",
+      #"echo sudo az login --service-principal --username $(ARM_CLIENT_ID) --password $(ARM_CLIENT_SECRET) --tenant $(ARM_TENANT_ID)",
       #"hostname > ~/ansible/hosts",
       #"cd ~/ansible",
       #"sudo ansible-playbook ~/ansible/proxy.yml -i ~/ansible/hosts"
