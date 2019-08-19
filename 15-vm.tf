16cbf676b7160b5869ea6d50f56fc636fd054634
stravze <stravze@gmail.com>
2019-08-19 14:39:12 +0100

    fixing variable

--- 15-vm.tf
+++ 15-vm.tf
@@ -92,7 +92,7 @@ resource "null_resource" "ansible-runs" {
     inline = [
       "hostname > ~/ansible/hosts",
       "cd ~/ansible",
-      "sudo ansible-playbook ~/ansible/proxy.yml -i ~/ansible/hosts -e arm_tenant_id=${var.arm_tenant_id} -e arm_client_id=${var.arm_client_id} -e arm_client_secret=${var.arm_client_secret} -e sub-hub-transit-public=${var.sub-hub-transit-public}",
+      "sudo ansible-playbook ~/ansible/proxy.yml -i ~/ansible/hosts -e arm_tenant_id=${var.arm_tenant_id} -e arm_client_id=${var.arm_client_id} -e arm_client_secret=${var.arm_client_secret} -e sub_hub_transit_public=${var.sub_hub_transit_public}",
     ]
 
     connection {
