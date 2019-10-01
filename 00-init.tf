d1edec97e6bb28f7dd05c6d0c21b32b7fbbecf96
stravze <stravze@gmail.com>
2019-10-01 17:06:14 +0100

    disable provider

--- 00-init.tf
+++ 00-init.tf
@@ -1,3 +1,4 @@
+/*
 terraform {
   required_version                  = ">= 0.11.0"
  backend "azurerm" {}
@@ -7,7 +8,7 @@ provider "azurerm" {
   version         = ">=1.24.0"
   subscription_id = "${var.subscription_id}"
 }
-
+*/
 
 
 
